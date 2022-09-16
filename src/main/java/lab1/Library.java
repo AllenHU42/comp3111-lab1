f48efb3b3ad37a8095aef02dfa86f1aa27abb31c
Allenboy42 <95038994+AllenHU42@users.noreply.github.com>
2022-09-16 17:32:05 +0800

    Add: Test change

--- src/main/java/lab1/Library.java
+++ src/main/java/lab1/Library.java
@@ -8,6 +8,9 @@ public class Library {
         return true;
     }
     public static void main(String arg[]) {
-    	System.out.println("Hello World");
+    	Library lib = new Library();
+    	if(lib.someLibraryMethod()) {
+    		System.out.println("Hello World");	
+    	}
     }
 }
